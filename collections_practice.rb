645de0dad60360035094655084934f133f9ee6e6
Eric DeSimone <eric.desimone@cortland.edu>
2018-04-12 17:33:50 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -32,7 +32,9 @@ end
 def kesha_maker(array)
   kesha = Array.new
   array.each do |word|
-    kesha << word(2) = "$"
+    word.chars
+    word[2].delete
+    kesha << word.insert(2,"$")
   end
   kesha
 end
