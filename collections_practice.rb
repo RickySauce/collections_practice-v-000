c17884b5256b977a5cc71cbfff2e1894bb9fa599
Eric DeSimone <eric.desimone@cortland.edu>
2018-04-12 18:27:53 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -52,8 +52,7 @@ end
 =end
 [11,4,7,8,9,100,134]
 def sum_array(array)
-  array.collect do |integer|
-    integer = array[0] + array[1]
+ until array.length < 2
+   array[0] = array[0] + array[1]
  end
-array
 end
