b7e41b8d70b4fee5c7a6880dd492cd65eb68a0f5
Eric DeSimone <eric.desimone@cortland.edu>
2018-04-12 18:35:40 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -52,9 +52,9 @@ end
 =end
 [11,4,7,8,9,100,134]
 def sum_array(array)
-  i = 0
-     while i < @a.length
-       @sum += @a[i]
-       i = i + 1
-     end
+sum = 0
+  array.each do |i|
+    sum +=
+  end
+  sum
 end
